e791b5976900d3d804e1054b8af0b1f0eca3e35a
Valek2412 <valoleg99@gmail.com>
2020-02-09 22:21:05 +0200

    added sorting

--- src/components/NewsTable.js
+++ src/components/NewsTable.js
@@ -3,14 +3,48 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import EnhancedTableHead from './EnhancedTableHead'
 import InfiniteScroll from 'react-infinite-scroller';
 import useRequest from '../hooks/useRequest';
 
 const NewsTable = () => {
   const [state, update] = useRequest('https://api.hnpwa.com/v0/news/1.json');
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const descendingComparator = (a, b, orderBy) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  };
+
+  const getComparator = (order, orderBy) => {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+  };
+
+  const stableSort = (array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map(el => el[0]);
+  };
 
   if (state.isFetching && (state.page === 1)) return (<div>Loading</div>);
 
@@ -24,15 +58,13 @@ const NewsTable = () => {
     >
       <TableContainer component={Paper}>
         <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Time added</TableCell>
-              <TableCell align="right">Title</TableCell>
-              <TableCell align="right">Domain</TableCell>
-            </TableRow>
-          </TableHead>
+          <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+          />
           <TableBody>
-            {state.responseData.map((item) => (
+            {stableSort(state.responseData, getComparator(order, orderBy)).map((item) => (
               <TableRow hover onClick={() => { window.location.href = item.url; }} key={item.id}>
                 <TableCell component="th" scope="row">
                   {item.time_ago}
